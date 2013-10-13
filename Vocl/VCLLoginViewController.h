c9eae184cdd90f81bfd76249ff1380dd46e9b736
Rick Roberts <elgreco84@gmail.com>
2013-10-13 16:26:22 -0600

    added pods

---
+++ Vocl/VCLLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCLLoginViewController.h
+//  Vocl
+//
+//  Created by Rick Roberts on 10/13/13.
+//  Copyright (c) 2013 Street Technology, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCLLoginViewController : UIViewController
+
+@end
---
+++ Vocl/VCLLoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  VCLLoginViewController.m
+//  Vocl
+//
+//  Created by Rick Roberts on 10/13/13.
+//  Copyright (c) 2013 Street Technology, LLC. All rights reserved.
+//
+
+#import "VCLLoginViewController.h"
+
+@interface VCLLoginViewController ()
+
+@end
+
+@implementation VCLLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)twitterLogin:(id)sender
+{
+    
+}
+
+@end
